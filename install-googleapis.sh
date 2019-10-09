0fcac88b4e9acb3c7d12edba6e1bc6ae99d59d60
Alpha <shydesky@gmail.com>
2019-10-09 17:08:43 +0800

    Update install-googleapis.sh

--- install-googleapis.sh
+++ install-googleapis.sh
@@ -2,7 +2,7 @@
 set -e
 
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+go get -u github.com/shydesky/grpc-gateway/protoc-gen-swagger
 go get -u github.com/golang/protobuf/protoc-gen-go
 
 wget http://central.maven.org/maven2/com/google/api/grpc/googleapis-common-protos/0.0.3/googleapis-common-protos-0.0.3.jar
